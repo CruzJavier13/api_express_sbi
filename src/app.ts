fea1736674516f8735d2335c7698ff381a85cdc8
Erick Cruz <erickcroz2013@gmail.com>
2025-05-08 10:25:28 -0600

    Add file public to root system for static file

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import routes from './routes/routes';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/', routes);
 
